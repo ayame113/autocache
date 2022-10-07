12525c1c33388e0bcf2068247e16231bdb0d4a0b
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-10-07 21:56:01 +0900

    readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # autocache
+
+experimental!!
+
+```ts
+import { serve } from "https://deno.land/std@0.158.0/http/mod.ts";
+import { withCache } from "./mod.ts";
+
+serve(withCache(() =>
+  Response.json({ a: "aa" }, {
+    headers: { "Cache-Control": "public, max-age=604800" },
+  })
+));
+```
--- test.ts
+++ test.ts
@@ -1,10 +1,8 @@
 import { serve } from "https://deno.land/std@0.158.0/http/mod.ts";
 import { withCache } from "./mod.ts";
 
-serve(
-  withCache(() =>
-    Response.json({ a: "aa" }, {
-      headers: { "Cache-Control": "public, max-age=604800" },
-    })
-  ),
-);
+serve(withCache(() =>
+  Response.json({ a: "aa" }, {
+    headers: { "Cache-Control": "public, max-age=604800" },
+  })
+));
